a61aa1d09520e2524aff260dd8f3c6252e2274ea
FlavioLSS <flavio.lucas.ss13@gmail.com>
2022-04-18 15:57:25 -0300

    movies update

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        <h1>Movies</h1>
-    </div>
+    <body>
+        <div class="container">
+            <h1>Movies</h1>
+        </div>
+    </body>
 </template>
+
+<style scoped>
+*{
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    text-decoration: none;
+}
+
+body{
+    background: url('../assets/bg-movies.jpg') no-repeat center fixed;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+
+.container{
+    background: white;
+    position:relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 1400px;
+}
+</style>
