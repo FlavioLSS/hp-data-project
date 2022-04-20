f07275c825c87e5c3bf3229c41023d825d25fc9b
FlavioLSS <flavio.lucas.ss13@gmail.com>
2022-04-20 01:55:18 -0300

    modelo modal aplicado

--- src/components/movieModals/movie-modal-10.vue
+++ src/components/movieModals/movie-modal-10.vue
@@ -1,9 +1,160 @@
 <template>
-    <div>
-        <h1>Movie 10</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto recusandae a cumque ratione doloremque voluptas aspernatur itaque! Harum, eum animi voluptas praesentium perferendis voluptates nihil? Maxime doloribus odio, aspernatur laudantium placeat itaque ipsum sed magnam dolorem ratione et optio, dolores tempore ipsa consectetur earum necessitatibus beatae rem molestias temporibus officia incidunt sint. Eaque atque ea incidunt cupiditate autem dolores praesentium aliquid assumenda et officia facere ex exercitationem, aspernatur soluta ducimus consequatur nam possimus sed optio libero voluptatum repellat. Ipsam eum eveniet minus, veritatis nobis error eaque laudantium voluptates, ad vitae perferendis, hic atque dignissimos? Corporis minima quod ad at.</h2>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto recusandae a cumque ratione doloremque voluptas aspernatur itaque! Harum, eum animi voluptas praesentium perferendis voluptates nihil? Maxime doloribus odio, aspernatur laudantium placeat itaque ipsum sed magnam dolorem ratione et optio, dolores tempore ipsa consectetur earum necessitatibus beatae rem molestias temporibus officia incidunt sint. Eaque atque ea incidunt cupiditate autem dolores praesentium aliquid assumenda et officia facere ex exercitationem, aspernatur soluta ducimus consequatur nam possimus sed optio libero voluptatum repellat. Ipsam eum eveniet minus, veritatis nobis error eaque laudantium voluptates, ad vitae perferendis, hic atque dignissimos? Corporis minima quod ad at.</h2>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto recusandae a cumque ratione doloremque voluptas aspernatur itaque! Harum, eum animi voluptas praesentium perferendis voluptates nihil? Maxime doloribus odio, aspernatur laudantium placeat itaque ipsum sed magnam dolorem ratione et optio, dolores tempore ipsa consectetur earum necessitatibus beatae rem molestias temporibus officia incidunt sint. Eaque atque ea incidunt cupiditate autem dolores praesentium aliquid assumenda et officia facere ex exercitationem, aspernatur soluta ducimus consequatur nam possimus sed optio libero voluptatum repellat. Ipsam eum eveniet minus, veritatis nobis error eaque laudantium voluptates, ad vitae perferendis, hic atque dignissimos? Corporis minima quod ad at.</h2>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto recusandae a cumque ratione doloremque voluptas aspernatur itaque! Harum, eum animi voluptas praesentium perferendis voluptates nihil? Maxime doloribus odio, aspernatur laudantium placeat itaque ipsum sed magnam dolorem ratione et optio, dolores tempore ipsa consectetur earum necessitatibus beatae rem molestias temporibus officia incidunt sint. Eaque atque ea incidunt cupiditate autem dolores praesentium aliquid assumenda et officia facere ex exercitationem, aspernatur soluta ducimus consequatur nam possimus sed optio libero voluptatum repellat. Ipsam eum eveniet minus, veritatis nobis error eaque laudantium voluptates, ad vitae perferendis, hic atque dignissimos? Corporis minima quod ad at.</h2>
+    <div class="modal">
+            <h2>Harry Potter e a Pedra Filosofal</h2>
+
+        <div class="part1">
+
+            <div class="trailer">
+
+                <iframe class="trailer-v" src="https://www.youtube.com/embed/9fIObnIGMlI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <v-row>
+                    <v-col col="6"><h3>Ano de Lançamento: 2001</h3></v-col>
+                    <v-col col="6"><h3>Diretor: Chris Columbus</h3></v-col>
+                </v-row>
+
+                 <v-row>
+                    <v-col col="6"><h3>Bilheteria: US$ 965 milhões</h3></v-col>
+                    <v-col col="6"><h3>Nota (Imdb): 7,6 / 10</h3></v-col>
+                </v-row>
+            </div>
+            <div class="sinopse">
+                     <h2>Sinopse</h2>
+                         <h3>Harry Potter vive com os tios Dursley, onde é mal tratado até completar seus 11 anos. É com essa idade que o jovem bruxo começa a receber cartas da escola de Hogwarts. Na noite de seu aniversário Harry é visitado por Hagrid (um ser gigante que trabalha para o diretor de Hogwarts), o qual revela que Harry é filho de bruxos e foi convidado a ingressar na escola de bruxaria. A verdade é toda revelada a Harry Potter, que seus pais foram mortos pelo terrível bruxo Voldemort e que a sua cicatriz era marca da tentativa de assassinato que Harry sofrera. Esse primeiro livro apresenta o leitor aos principais personagens da série e cria uma aura de “simpatia” pelo personagem principal (Harry).</h3>
+                    </div>
+
+        </div>
+
+                    <h2>Momentos Icônicos</h2>
+    <div class="part2">
+
+            <div class="momento">
+                <h2>Rony se sacrificando na partida de xadrez bruxo</h2>
+                <iframe class="trailer-v2" src="https://www.youtube.com/embed/9fIObnIGMlI" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="artigo">
+                         <h3>Harry Potter vive com os tios Dursley, onde é mal tratado até completar seus 11 anos. É com essa idade que o jovem bruxo começa a receber cartas da escola de Hogwarts. Na noite de seu aniversário Harry é visitado por Hagrid (um ser gigante que trabalha para o diretor de Hogwarts), o qual revela que Harry é filho de bruxos e foi convidado a ingressar na escola de bruxaria. A verdade é toda revelada a Harry Potter, que seus pais foram mortos pelo terrível bruxo Voldemort e que a sua cicatriz era marca da tentativa de assassinato que Harry sofrera. Esse primeiro livro apresenta o leitor aos principais personagens da série e cria uma aura de “simpatia” pelo personagem principal (Harry).</h3>
+                    </div>
+             </div>
+              <div class="part2">
+
+            <div class="momento">
+                <h2>Rony se sacrificando na partida de xadrez bruxo</h2>
+                <iframe class="trailer-v2" src="https://www.youtube.com/embed/9fIObnIGMlI" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="artigo">
+                         <h3>Harry Potter vive com os tios Dursley, onde é mal tratado até completar seus 11 anos. É com essa idade que o jovem bruxo começa a receber cartas da escola de Hogwarts. Na noite de seu aniversário Harry é visitado por Hagrid (um ser gigante que trabalha para o diretor de Hogwarts), o qual revela que Harry é filho de bruxos e foi convidado a ingressar na escola de bruxaria. A verdade é toda revelada a Harry Potter, que seus pais foram mortos pelo terrível bruxo Voldemort e que a sua cicatriz era marca da tentativa de assassinato que Harry sofrera. Esse primeiro livro apresenta o leitor aos principais personagens da série e cria uma aura de “simpatia” pelo personagem principal (Harry).</h3>
+                    </div>
+             </div>
+              <div class="part2">
+
+            <div class="momento">
+                <h2>Rony se sacrificando na partida de xadrez bruxo</h2>
+                <iframe class="trailer-v2" src="https://www.youtube.com/embed/9fIObnIGMlI" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="artigo">
+                         <h3>Harry Potter vive com os tios Dursley, onde é mal tratado até completar seus 11 anos. É com essa idade que o jovem bruxo começa a receber cartas da escola de Hogwarts. Na noite de seu aniversário Harry é visitado por Hagrid (um ser gigante que trabalha para o diretor de Hogwarts), o qual revela que Harry é filho de bruxos e foi convidado a ingressar na escola de bruxaria. A verdade é toda revelada a Harry Potter, que seus pais foram mortos pelo terrível bruxo Voldemort e que a sua cicatriz era marca da tentativa de assassinato que Harry sofrera. Esse primeiro livro apresenta o leitor aos principais personagens da série e cria uma aura de “simpatia” pelo personagem principal (Harry).</h3>
+                    </div>
+             </div>
+
     </div>
+    
 </template>
+
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+h2{
+    color: white;
+    font-size: 42px;
+    font-family: 'Cinzel Decorative', 'cursive';
+    margin: 40px 0;
+}
+
+h3{
+    color: white;
+    font-family: 'Bitter', serif;
+    font-weight: normal;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+}
+
+.modal{
+    width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background: rgb(17, 16, 16);
+}
+.modal h2{
+    border-bottom: 2px solid gray;
+}
+.part1{
+    background: rgba(0, 0, 0, 0.870);
+    border-radius: 10px;
+    width: 1280px;
+    display: flex;
+}
+
+.sinopse{
+    text-align: left;
+    max-width: 600px;
+}
+
+.sinopse h2{
+    font-size: 28px;
+    font-family: 'Bitter', serif;
+    text-transform: uppercase;
+    font-weight: normal;
+    border-bottom: 2px solid white;
+}
+
+.trailer{
+    margin: 20px;
+}
+
+.trailer h3{
+    text-align: left;
+    margin-left: 5px;
+    border-bottom: 1px solid white;
+    border-right: 1px solid white;
+}
+
+.trailer-v{
+    width: 640px;
+    height: 415px;
+}
+
+.part2{
+    background: rgba(0, 0, 0, 0.870);
+    border-radius: 10px;
+    width: 1280px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.trailer-v2{
+    width: 960px;
+    height: 615px;
+}
+
+.momento h2{
+    font-family: 'Bitter', serif;
+    font-size: 32px;
+    text-transform: uppercase;
+    font-weight: normal;
+    border-right: 2px solid white;
+    border-left: 2px solid white;
+    border-bottom: none;
+}
+
+.artigo{
+    width: 960px;
+    text-align: left;
+    margin-bottom: 30px;
+}
+</style>
